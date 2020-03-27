e46c417ce5ec5d8a9192692f689b0faf33cef993
Renan-NO <re.office.25@gmail.com>
2020-03-27 16:12:41 -0300

    Mobile UP

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
+
+
+// Tds os elementos no react-native são display flex
+// Não existe herança de estilos no react-native, o estilo é dado por componente ou por elemento 
